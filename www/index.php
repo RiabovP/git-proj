87080ffdb2b4c54496e634bfe365d3380ba04e39
Pavel <riabov_84@mail.ru>
2016-09-26 21:09:46 +0700

    commit new_f

--- www/index.php
+++ www/index.php
@@ -1,4 +1,13 @@
-﻿<?php
+﻿<!Doctype>
+<HTML>
+	<head>
+		<meta charset=UTF8 content="масло двигатель коробка mobil" name="">
+		<title>
+			ООО "СТЕЛС"
+		</title>
+		<link rel="stylesheet" href="style1.css" type="text/css">
+	</head>
+<?php
 echo "<h1 align=center> Привет нет Мир нет WORLD </h1>";
 echo "<h3 align=center> Правка для git </h1>";
 ?>
