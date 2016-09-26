9d90e3bb81184ddf6feeddd496874d2ff87ddec4
Pavel <riabov_84@mail.ru>
2016-09-26 22:23:42 +0700

    commit new

--- www/index.php
+++ www/index.php
@@ -14,5 +14,6 @@
 echo "<h1 align=center> Привет нет Мир нет WORLD </h1>";
 echo "<h3 align=center> Правка для git </h1>";
 echo "<h2 align=center> Восстановление из другой ветви</h2>";
+echo "<h1 align=center> Восстановление прошло успешно</h1>";
 ?>
 </body>
